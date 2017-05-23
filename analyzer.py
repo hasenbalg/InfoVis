48181baf9d39de8d83f37aaec84deee62ec3827c
frank <frankam@inbox.lv>
2017-05-23 09:57:27 +0200

    restructure

--- analyzer.py
+++ analyzer.py
@@ -1,140 +1,173 @@
-import re
+# class Text(object):
+#     def __init__(self):
+#         self.paragraphs = []
+#
+#     def add_paragraph(self, paragraph):
+#         self.paragraphs.append(paragraph)
+#
+# class Paragraph(object):
+#     def __init__(self):
+#         self.heading
+#         self.sentences = []
+#
+#     def add_heading(self, heading):
+#         self.heading = heading
+#
+#     def add_sentence(self, sentence):
+#         self.sentences.append(sentence)
+#
+# class Sentence(object):
+#     def __init__(self):
+#         self.words = []
+#
+# class Word(object):
+#     def __init__(self):
+#         self.word
+#         self.positions = []
+#         self.idf
+
 import sys
-class Lemma(object):
-
-    def __init__(self,name, position):
-        self.name = name
-        self.positions = [position]
-    def add_position(self, position):
-        self.positions.append( position)
-
-    def and_where(self):
-        output = self.name + " "
-        for pos in self.positions:
-            output += " " + str(pos)
-        #output += "\n"
-        return output;
-    @staticmethod
-    def stemming(word):
-        word = re.sub("s$","",word)
-        word = re.sub("ing$","",word)
-        word = re.sub("ed$","",word)
-        word = re.sub("ly$","",word)
-        # word = re.sub("ish$","",word)
-        return word
-
-    def idf(self, lemmata, text_length):
-        most_common_lemma_pos_len = 0;
-        for lemma in lemmata:
-            if len(lemma.positions) > most_common_lemma_pos_len :
-                most_common_lemma_pos_len =len(lemma.positions)
-        df = len(self.positions)/float(text_length)
-        most_df = most_common_lemma_pos_len/float(text_length)
-        return df/most_df
+import re
+import html.parser
+from nltk.tokenize import word_tokenize
+from nltk.text import Text
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from nltk.tokenize import sent_tokenize
 
 
+paragraph_end_token = 'xxxendparagraphxxx'
+heading_end_token = 'xxxendheadingxxx'
 
-#########################################################
-def get_avg_word_distance(word1, word2, text):
-    if word1 is word2:
-        return
 
-    distances = []
-    counter = 0;
-    counting = False
-    # foreward
-    for w in text:
-        if w in word1:
-            counting = True
-        if w in word2:
-            counting = False
-            if counter > 0:
-                distances.append(counter)
-                counter = 0
-        if counting:
-            counter +=1
-    # backward
-    for w in text:
-        if w in word2:
-            counting = True
-        if w in word1:
-            counting = False
-            if counter > 0:
-                distances.append(counter)
-                counter = 0
-        if counting:
-            counter +=1
-    # print distances
-    return sum(distances) / float(len(distances))
+def make_json_from_tuple(list, textList):
+    # make freqs
+    max_occurence = 0;
+    for t in list:
+        if t[1] > max_occurence:
+            max_occurence = t[1]
+    max_tf = max_occurence/len(textList)
+    output = ""
+    for t in list:
+        # https://mkaz.tech/code/python-string-format-cookbook/
+        output += "{" + ("'word': '{}', 'idf': {}".format(t[0], (t[1]/len(textList))/max_tf)) + "},"
+    return "var frequencies = [" + output.rstrip(',') + "];"
 
 
 
 def main():
-    text = [];
+    if not sys.argv[1]:
+        return
+    # read text
+    text = open(sys.argv[1], "r").read()
+
+    # remove script
+    # text = re.sub(r'<script.+?>(.+?)</script>', ' ', text)
+    text = re.sub(r'<script[\s\S]+?</script>', ' ', text)
+
+    # set *end_tokens
+    text = re.sub(r'</p>',' ' +  paragraph_end_token + ' ', text)
+    text = re.sub(r'</h[\d]>', ' ' +  heading_end_token + ' ', text)
+    #remove tags
+    text = re.sub(r'<.+?>', ' ', text)
+    text = html.parser.HTMLParser().unescape(text)
+    text_to_chop_in_sentences = text # for later use
+    text = text.lower()
+    # remove quote referneces
+    text = re.sub(r'\[.+?\]', ' ', text)
+    # remove html comments
+    text = re.sub(r'<!--[^>]*-->', ' ', text)
+
+    # print (text)
+
+    # tokenize the text
+    # https://pythonprogramming.net/tokenizing-words-sentences-nltk-tutorial/
+    # http://www.nltk.org/book/ch03.html
+    tokens = word_tokenize(text)
+    textList = Text(tokens)
+
+
+    # find full stops and paragraphs
+    full_stops = []
+    paragraphs = []
+    for i in range(0, len(textList)):
+        if textList[i] is '.':
+            full_stops.append(i)
+
+        if textList[i] in paragraph_end_token:
+            paragraphs.append(i)
+
+    print('var title = "' + str(sys.argv[1]) + '";')
+    print('var full_stops = ' + str(full_stops) + ';')
+    print('var paragraphs = ' + str(paragraphs) + ';')
+
+    # remove puctuation and *end_tokes
+    textList_with_punctuation = textList
+    textList = [w for w in textList if w.isalnum() and w not in [paragraph_end_token ,heading_end_token]]
+
+    # print('var text_length = ' + str(len(textList)) + ';')
+
+    # Lemmatize
+    lemmatizer = WordNetLemmatizer()
     lemmata = []
-    static_stop_words = ["an","are","was","at","with","from", "can", "its", "they","were", "other", "more","it","by","is", "after","for","that","on","up","in","to","and","a","of","the","or","he","she","nor","ether","their","that","this","you","then","than","if","when","as", "has", "be", "have", "been", "which", "his", "her"]
+    for word in textList:
+        lemmata.append(lemmatizer.lemmatize(word))
 
-    for line in open(sys.argv[1], "r"):
-        line = line.strip()
-        if len(line) > 0:
-            for word in line.split():
-                    word  = filter(str.isalnum, word).lower()
-                    text.append(word)
+    # get lemmata in order of freq and get rid of crippled words(length < 2)
+    # http://www.nltk.org/book/ch01.html
+    fdist1 = FreqDist([w.lower() for w in lemmata if w not in set(stopwords.words('english')) and len(w) > 2])
 
-    print str(len(text)) + " Woerter gefunden"
+    histogram = make_json_from_tuple(fdist1.most_common(50), textList)
+    print(histogram)
 
-    #collect double words
-    i = 0;
-    for word in text:
-        allready_exists = False
-        if word not in static_stop_words and len(word) > 1:
-            word = Lemma.stemming(word)
-            for w in lemmata:
-                if w.name == word:
-                    w.add_position(i)
-                    allready_exists = True
-            if not allready_exists:
-                lemmata.append(Lemma(word, i))
-        i+=1
+    # force
+    word_list = [w[0] for w in fdist1.most_common(10)]
+    nodes = ''
+    for word in word_list:
+        nodes += "{'name': '" + word + "' },"
 
-    print str(len(lemmata)) + " unterschiedliche Woerter gefunden"
+    links = ''
+    sentences_list = sent_tokenize(text_to_chop_in_sentences)
 
-    #sort lemmata
-    lemmata = sorted(lemmata, key=lambda word: len(word.positions))
-    #find stop words
-    # stop_words = []
-    # for word in lemmata:
-    #     if word.idf(lemmata, len(text)) >= .076:
-    #         stop_words.append(word)
-    # print str(len(stop_words)) + " stop words found"
+    for i in range(len(word_list)):
+        for j in range(len(word_list)):
+            if word_list[i] is word_list[j]:
+                continue
+            counter = 0
+            for sentence in sentences_list:
+                if word_list[i] and word_list[j] in sentence:
+                    counter += 1
+            if counter > 0:
+                links += "{'source':" + str(i) + ",'target':" + str(j) + ",'value':" + str(counter) + "},"
 
-    #remove stop words
-    # for word in lemmata:
-    #     for stop_word in stop_words:
-    #         if word.name == stop_word.name:
-    #             lemmata.remove(word)
-    # print len(lemmata)
+    print("var force_data = {'nodes': [" + nodes + "], 'links': [" + links + "]};")
 
-    #sort stop words
-    # stop_words = sorted(stop_words, key=lambda word: len(word.positions))
-    # for word in stop_words:
-    #     print word.name
+    # dispersion plot
+    word_list = [w[0] for w in fdist1.most_common(10)]
+    word_list.extend([paragraph_end_token, '.'])
+    dispositions = ''
+    for word1 in word_list:
+        counter = 0
+        indecies = []
+        for word2 in textList_with_punctuation:
+            if word1 in word2:
+                indecies.append(counter)
+            counter += 1
+        dispositions += "{name : '" + word1 + "', positions: " + str(indecies) + "},"
 
-    for lemma in lemmata[-9:]: # last 10 of list
-        print "{"
-        print "\"name\": " + "\"" + lemma.name + "\"" + ","
-        print "\"idf\": " + str(lemma.idf(lemmata, len(text))) + ","
-        print "\"avg_distance\": " + str(get_avg_word_distance(lemmata[len(lemmata)-1].name, lemma.name, text))
-        print "},"
+    # dispositions += "{name : 'huhu', positions: [1,3,5,7,9, 10]},{name : 'haha', positions: [2,4,6,8]},{name : 'hihi', positions: [3,4,7,15]},{name : 'hoho', positions: [3,4,7,15, 17, 12]}"
+    print("var dispositions = [" + dispositions + "];")
+    print("var text_length = " + str(len(textList)) + ";")
 
-
-    # print lemmata[len(lemmata)-1].name, lemmata[len(lemmata)-2].name
-    # print(get_avg_word_distance(lemmata[len(lemmata)-1].name, lemmata[len(lemmata)-2].name, text))
-    # print lemmata[len(lemmata)-1].name, lemmata[len(lemmata)-3].name
-    # print(get_avg_word_distance(lemmata[len(lemmata)-1].name, lemmata[len(lemmata)-3].name, text))
-    # print lemmata[len(lemmata)-1].name, lemmata[len(lemmata)-4].name
-    # print(get_avg_word_distance(lemmata[len(lemmata)-1].name, lemmata[len(lemmata)-4].name, text))
-
+    #mini map
+    output = []
+    for word in textList:
+        if word in [paragraph_end_token, heading_end_token]:
+            output.append('\n')
+        else:
+            output.append(word)
+    print("var mini_map = " + str(output) + ";")
 
 if __name__ == '__main__':
     main()
